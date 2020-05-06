5ce075f35f2d45819214740fd906aae5eccf8779
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-06 01:32:00 -0300

    Adicionada uma transição para o menu responsivo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var button = document.querySelector('.topbar__hamburger');
 var navbar = document.querySelector('.topbar__nav');
+var listItens = document.querySelectorAll('.nav__item');
 function changeMenu(){
-    navbar.classList.toggle('topbar__nav--closed');
+    navbar.classList.toggle('topbar__nav--active');
 }
-
+function closeMenu(){
+    if(navbar.classList.contains('topbar__nav--active')){
+        navbar.classList.toggle('topbar__nav--active');
+    }
+}
 button.addEventListener('click',changeMenu);
+listItens.forEach(item=>{
+    item.addEventListener('click',closeMenu);
+});
