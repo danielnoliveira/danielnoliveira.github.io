e09c748d4bb45a555360e280a8bc50f91ecaacd0
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-09 10:52:00 -0300

    Configuração das animações. Scroll horizontal feito para dispositivos moveis na area de Skills

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var button = document.querySelector('.topbar__hamburger');
-var navbar = document.querySelector('.topbar__nav');
-var listItens = document.querySelectorAll('.nav__item');
-function changeMenu(){
-    navbar.classList.toggle('topbar__nav--active');
-}
-function closeMenu(){
-    if(navbar.classList.contains('topbar__nav--active')){
+var button = null;
+var navbar = null;
+var listItens = null;
+
+
+    button = document.querySelector('.topbar__hamburger');
+    navbar = document.querySelector('.topbar__nav');
+    listItens = document.querySelectorAll('.nav__item');
+
+    function changeMenu(){
         navbar.classList.toggle('topbar__nav--active');
     }
-}
-button.addEventListener('click',changeMenu);
-listItens.forEach(item=>{
-    item.addEventListener('click',closeMenu);
-});
+    function closeMenu(){
+        if(navbar.classList.contains('topbar__nav--active')){
+            navbar.classList.toggle('topbar__nav--active');
+        }
+    }
+    button.addEventListener('click',changeMenu);
+    listItens.forEach(item=>{
+        item.addEventListener('click',closeMenu);
+    });
+
